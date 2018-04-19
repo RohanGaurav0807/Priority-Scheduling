5a8786c6a23dfa6f8b7360e76eb300909d3895cf
RohanGaurav0807 <38460907+RohanGaurav0807@users.noreply.github.com>
2018-04-19 17:14:13 +0530

    Update Rohan_os_project.c

--- Rohan_os_project.c
+++ Rohan_os_project.c
@@ -1,102 +1,114 @@
-
-#include<stdio.h> 
- 
-int main() 
-{ 
-      int i, number, total = 0, x, counter = 0, TIME_QUANTUM; 
-      int sum=0,p;
-	  
-	  int WAIT = 0, TAT = 0, Arive[10], Burst[10], M[10],priority[20],m[20]; 
-      	printf("************************WELCOME*****************************************\n\n");
-      	
-		  	
-	  float AVG_WAIT, AVG_TAT;
-      printf("\nEnter Total Number of Processes:"); 
-      scanf("%d", &number); 
-      x = number; 
-      
-      for(i = 0; i < number; i++) 
-      {
-      
-            printf("\nEnter Details of Process[%d]\n", i + 1);
-         
-		    printf("Arrival Time:\t");
-         
-		    scanf("%d", &Arive[i]);
-         
-		    printf("Burst Time:\t");
-         
-		    scanf("%d", &Burst[i]);
-			
-			printf("Priority:\t");
-            
-			scanf("%d",&priority[i]); 
-            
-            sum= sum+Burst[i];
-      }   
-		
-     
-	
-		
-		p = sum/number;
-		printf("AVG of Burst Time : %d\n",p);
-		for(i=0;i<number;i++)
-		{
-			M[i]=Burst[i]%p;
-		}
-		
-		
-		
-		
-		TIME_QUANTUM=p;
-		printf("Time Quantum is : %d\n\n",TIME_QUANTUM);
-	
-		printf("\nProcess ID\tArrival Time\t\tBurst Time\t Priority\t\t Turnaround Time\t Waiting Time\n");
-      
-	
-	  for(total = 0, i = 0; x != 0;) 
-      { 
-     
-           
-		    if(M[i] <= TIME_QUANTUM && M[i] > 0) 
-            { 
-                  total = total + M[i]; 
-                  M[i] = 0; 
-                  counter = 1; 
-            } 
-            else if(M[i] > 0) 
-            { 
-                  M[i] = M[i] - TIME_QUANTUM; 
-                  total = total + TIME_QUANTUM; 
-            } 
-            if(M[i] == 0 && counter == 1) 
-            { 
-                  x--; 
-                  printf("\nProcess[%d]\t%d\t\t\t%d\t\t %d\t\t\t %d\t\t\t%d", i + 1,Arive[i], Burst[i],priority[i], total - Arive[i], total - Arive[i] - Burst[i]);
-                  WAIT = WAIT + total - Arive[i] - Burst[i]; 
-                  TAT = TAT + total - Arive[i]; 
-                  counter = 0; 
-            } 
-            if(i == number - 1) 
-            {
-                  i = 0; 
-            }
-            else if(Arive[i + 1] <= total) 
-            {
-                  i++;
-            }
-            else 
-            {
-                  i = 0;
-            }
-      } 
-	
-      AVG_WAIT = WAIT * 1.0 / number;
-      
-	  AVG_TAT = TAT * 1.0 / number;
-      
-	  printf("\n\nAVG Waiting Time:\t%f", AVG_WAIT); 
-      
-	  printf("\nAvg Turnaround Time:\t%f\n", AVG_TAT); 
-      return 0; 
+#include<stdio.h>
+#include<conio.h>
+struct process
+{
+    int id;
+    int arrival;
+    int burst;
+    int rem;
+    int wait;
+    int finish;
+    int ti;
+    int turnaround;
+    float ratio;
+}p1[10];
+int no,k;
+int checkp1(int s)
+{
+ int i;
+ for(i = 1; i <= s; i++)
+ {
+  if(p1[i].rem != 0)
+   return 1;
+ }
+ return 0;
 }
+int nextp1(int k)
+{
+ int i;
+ i=k+1;
+ while(checkp1(i) && i!=k)
+ {
+  if(p1[i].rem != 0)
+   return i;
+   else
+   i=(i+1)%no;
+ }
+}
+int main()
+{
+ int i,j,t,time = 0,n;
+ struct process temp;
+ int nextp1(int);
+ printf("\n \n Enter the number of process: ");
+ scanf("%d", &n);
+ printf("\n \n Enter the time quantum: ");
+ scanf("%d", &t);
+ for(i = 1; i <= n; i++)
+ {
+  p1[i].id = i;
+  printf("\n\nEnter the arrival time for p1 %d: ", i);
+  scanf("%d", &(p1[i].arrival));
+  printf("\nEnter the burst time for p1 %d: ", i);
+  scanf("%d", &(p1[i].burst));
+  p1[i].rem = p1[i].burst;
+  p1[i].ti=0;
+  p1[i].wait=0;
+  p1[i].finish=0;
+ }
+ for(i = 1; i <= n; i++)
+ {
+  for(j = i + 1; j <= n; j++)
+  {
+   if(p1[i].arrival > p1[j].arrival)
+   {
+   	temp = p1[i];
+    p1[i] = p1[j];
+    p1[j] = temp;
+   }
+  }
+ }
+
+ no = 0;
+ j = 1;
+
+ while(checkp1(n) == 1)
+ {
+  if(p1[no + 1].arrival == time)
+   no++;
+  if((p1[j].ti<=t)&&(p1[j].rem !=0))
+  {
+   p1[j].rem--;
+   p1[j].ti++;
+   for(i = 1; i <= no; i++)
+   {
+    if((i!=j) && (p1[i].rem != 0))
+     p1[i].wait++;
+   }
+  }
+  if(p1[j].rem==0)
+   p1[j].finish=time;
+  if((p1[j].ti >= t)||(p1[j].rem==0))
+  {
+   p1[j].ti = 0;
+   j=nextp1(j);
+  }
+  time++;
+  }
+ p1[n].finish = time;
+ printf("\n\n p1  Arrival  Burst   Waiting  Finishing turnaround  Tr/Tb \n");
+ printf("%5s %9s %7s %10s %8s %9s\n\n", "id", "time", "time", "time", "time", "time");
+ for(i = 1; i <= n; i++)
+ {
+  p1[i].turnaround = p1[i].wait + p1[i].burst;
+  p1[i].ratio = (float)p1[i].turnaround / (float)p1[i].burst;
+  printf("%5d %8d %7d  %8d %10d %9d %10.1f ", p1[i].id, p1[i].arrival,
+                          p1[i].burst,
+                          p1[i].wait, p1[i].finish,
+                          p1[i].turnaround, p1[i].ratio);
+
+  printf("\n\n");
+ }
+ getch();
+}
+
